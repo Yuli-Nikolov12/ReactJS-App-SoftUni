557a407ec24fcc0dc7140533f0825e0f636fb2bd
Yuli-Nikolov12 <yuli.nikolov99@gmail.com>
2024-07-18 18:56:50 +0300

    creating NotFound Page and adding Style to the component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route} from 'react-router-dom'
 import Home from "./components/Home";
+import Header from './components/Header'
+import NotFoundPage from "./components/NotFoundPage";
 
 export default function App() {
 
   return (
-    <>
-      <Home />
-    </>
+    <div className="bg-black">
+        <Header />
+        
+        <Routes>
+          <Route path="/*" element={<NotFoundPage />} />
+          <Route path="/" element={<Home />} />
+
+        </Routes>
+      </div>
   )
 }
---
+++ client/src/components/NotFoundPage.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+export default function NotFoundPage() {
+    return (
+      <>
+        <main className="min-h-full px-6 py-24 sm:py-32 lg:px-8">
+            <div  className="text-center p-20 ">
+                <p className="text-3xl text-base font-semibold text-indigo-600">404</p>
+                <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-300 sm:text-5xl">Page not found</h1>
+                <p className="mt-6 text-base leading-7 text-gray-300">Sorry, we couldn’t find the page you’re looking for.</p>
+                    <div className="mt-10 flex items-center justify-center gap-x-6">
+                        <Link to="/"
+                        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Go back home
+                        </Link>
+                    </div>
+            </div>
+            <div
+            aria-hidden="true"
+            className="overflow-hidden blur-3xl "
+            >
+                <div
+                style={{
+                clipPath:
+                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                }}
+                className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+                />
+            </div>
+        </main>
+      </>
+    )
+  }
