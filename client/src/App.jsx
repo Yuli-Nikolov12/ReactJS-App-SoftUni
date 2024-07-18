e95b3e282fc01f21280d868eb0a45066662b8702
Yuli-Nikolov12 <yuli.nikolov99@gmail.com>
2024-07-18 16:09:27 +0300

    adding html template from Tailwind and installing the react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
+import Home from "./components/Home";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
     <>
-      <h1>Vite + React!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Home />
     </>
   )
 }
-
-export default App
