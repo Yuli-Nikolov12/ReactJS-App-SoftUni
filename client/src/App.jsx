4d6981abda1b83a9796d742d899363bce4f8ded8
Yuli-Nikolov12 <yuli.nikolov99@gmail.com>
2024-07-19 15:16:05 +0300

    creating register form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Header from './components/Header'
 import NotFoundPage from "./components/NotFoundPage";
 import Login from './components/Login';
+import Register from './components/Register';
 
 export default function App() {
 
@@ -14,6 +15,7 @@ export default function App() {
           <Route path="/*" element={<NotFoundPage />} />
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
   )
